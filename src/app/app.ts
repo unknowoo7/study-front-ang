6ad2808b72ae54f07e924ee8e7449055cd56b261
a.kazakov <a.kazakov@bidzaar.com>
2025-08-01 08:50:03 +0300

    https://angular.dev/guide/forms/template-driven-forms\#add-a-new-actor

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,9 @@
 import {Component} from '@angular/core';
-import {ReactiveFavoriteColor} from './reactive-favorite-color/reactive-favorite-color';
-import {ProfileEditor} from './profile-editor/profile-editor';
 import {ActorForm} from './actor-form/actor-form';
 
 @Component({
   selector: 'app-root',
-  imports: [ReactiveFavoriteColor, ProfileEditor, ActorForm],
+  imports: [ActorForm],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
